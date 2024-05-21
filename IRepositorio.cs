65d18324668e70a85ffcee614420a0c6df245246
Juan Quesada <juan.paq72@gmail.com>
2024-05-21 11:47:12 -0600

    Clase producto, interfas repositorioInventario y su implimentacion

---
+++ IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioDeProductos
+{
+    internal interface IRepositorio
+    {
+        void Agregar(Producto producto);
+        Producto Obtener(int id);
+        IEnumerable<Producto> ListarTodos();
+    }
+}
---
+++ Repositorio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioDeProductos
+{
+    internal class Repositorio
+    {
+        private List<Producto> _productos = new List<Producto>();
+        public void Agregar(Producto producto)
+        {
+
+
+        }
+
+        public IEnumerable<Producto> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Producto Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
